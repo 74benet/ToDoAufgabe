b13ebd0643e5956df65d39c92bb59e323e871eda
Benet Restelica <benet.restelica@gmail.com>
2024-09-27 10:08:08 +0200

    Input-Text

--- Aufgabe0/app/src/main/java/com/example/einfuehrung/MainActivity.kt
+++ Aufgabe0/app/src/main/java/com/example/einfuehrung/MainActivity.kt
@@ -6,9 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.einfuehrung.ui.theme.EinfuehrungTheme
@@ -20,10 +24,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             EinfuehrungTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Greeting(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +32,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(modifier: Modifier = Modifier) {
+    // State to store the text input
+    val textState = remember { mutableStateOf("") }
+
+    // UI with a TextField and a Text that displays the input
+    Column(modifier = modifier) {
+        TextField(
+            value = textState.value,
+            onValueChange = { newText ->
+                textState.value = newText
+            },
+            label = { Text("Enter your name") }
+        )
+        Text(text = "Hello ${textState.value}!")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     EinfuehrungTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
